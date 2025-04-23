2b0ee12fb9429bf1249891aecd25a5a73fa72a1f
Alexandra Vermillion <vermillionxela@gmail.com>
2025-04-22 19:09:40 -0600

    Added the SlayerRegisterView logic and form, added to routes, and started css module. Needs a lot more fine tuning but will be coming back to it after I finish some more functionality for other views/components tomorrow. Decided not to use bootstrap because of time constraints. Just using CSS modules now.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
+import {SlayerRegisterView} from './views/SlayerRegisterView'
 
 function App() {
 
 
   return (
     <>
+      <Routes>
+        <Route path="/slayer/register" element={<SlayerRegisterView />} />
+      </Routes>
     </>
   )
 }
--- client/src/views/SlayerRegisterView.jsx
+++ client/src/views/SlayerRegisterView.jsx
@@ -1,1 +1,153 @@
-//Form to create new Slayer
+import React, { useState } from 'react'
+import { registerSlayer } from '../services/slayer.service'
+import { useNavigate } from 'react-router-dom'
+import styles from '../css/SlayerForm.module.css'
+import { GiMagicSwirl, GiWolfHowl, GiPotionBall, GiSwordman, GiMantrap, GiFootsteps } from 'react-icons/gi'
+
+export const SlayerRegisterView = () => {
+    const DEFAULT_SLAYER_DATA = {
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        bio: '',
+        skills: [],
+    }
+
+    const SLAYER_SKILLS = [
+        { name: 'Magic', icon: <GiMagicSwirl /> },
+        { name: 'Beast Taming', icon: <GiWolfHowl /> },
+        { name: 'Alchemy', icon: <GiPotionBall /> },
+        { name: 'Weapons', icon: <GiSwordman /> },
+        { name: 'Trap Setting', icon: <GiMantrap /> },
+        { name: 'Tracking', icon: <GiFootsteps /> }
+    ]
+
+    const [newSlayer, setNewSlayer] = useState(DEFAULT_SLAYER_DATA)
+    const [errors, setErrors] = useState({})
+    const navigate = useNavigate()
+
+    const updateSlayer = (e) => {
+        const { name, value } = e.target
+        setNewSlayer(prev => ({ ...prev, [name]: value }))
+    }
+    
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        registerSlayer(newSlayer)
+            .then(() => {
+                setNewSlayer(DEFAULT_SLAYER_DATA)
+                setErrors({})
+                navigate('/slayer/dashboard')
+            })
+            .catch(errors => setErrors(errors))
+    }
+
+    const toggleSkill = (skillName) => {
+        setNewSlayer(prev => {
+            const alreadySelected = prev.skills.includes(skillName)
+
+            const updatedSkills = alreadySelected
+                ? prev.skills.filter(skill => skill !== skillName)
+                : [...prev.skills, skillName]
+
+            return { ...prev, skills: updatedSkills }
+        })
+    }
+
+    return (
+        //TODO: all css. make icons responsive
+        <div className={styles.formContainer}>
+            <h1>New Slayer Profile</h1>
+
+            <form onSubmit={handleSubmit}>
+
+                <div className={styles.leftContainer}>
+                    <div className={styles.formInput}>
+                        <label>Name:
+                            <input
+                                type="text"
+                                name="name"
+                                value={newSlayer.name}
+                                onChange={updateSlayer}
+                            />
+                        </label>
+                        {errors.name && <p className={styles.errorText}>{errors.name.message}</p>}
+                    </div>
+
+                    <div className={styles.formInput}>
+                        <label>Email:
+                            <input
+                                type="email"
+                                name="email"
+                                value={newSlayer.email}
+                                onChange={updateSlayer}
+                            />
+                        </label>
+                        {errors.email && <p className={styles.errorText}>{errors.email.message}</p>}
+                    </div>
+
+                    <div className={styles.formInput}>
+                        <label >Password:
+                            <input
+                                type="password"
+                                name="password"
+                                value={newSlayer.password}
+                                onChange={updateSlayer}
+                            />
+                        </label>
+                        {errors.password && <p className={styles.errorText}>{errors.password.message}</p>}
+                    </div>
+
+                    <div className={styles.formInput}>
+                        <label>Confirm Password:
+                            <input
+                                type="password"
+                                name="confirmPassword"
+                                value={newSlayer.confirmPassword}
+                                onChange={updateSlayer}
+                            />
+                        </label>
+                        {errors.confirmPassword && <p className={styles.errorText}>{errors.confirmPassword.message}</p>}
+                    </div>
+
+                    <div className={styles.formInput}>
+                        <label>Short Bio:
+                            <textarea
+                                name="bio"
+                                value={newSlayer.bio}
+                                onChange={updateSlayer}
+                            />
+                        </label>
+                        {errors.bio && <p className={styles.errorText}>{errors.bio.message}</p>}
+                    </div>
+                </div>
+
+
+                <div className={styles.rightColumn}>
+                    <h2>Hunting Skills</h2>
+                    <div className={styles.skillContainer}> 
+                    
+                        {SLAYER_SKILLS.map(({ name, icon }) => (
+                            <div
+                                key={name}
+                                onClick={() => toggleSkill(name)}
+                            >
+                                <div className={styles.icon}>
+                                    {icon}
+                                </div>
+                                <div className={styles.skillName}> 
+                                    {name}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    
+                    {errors.skills && <p className={styles.errorText}>{errors.skills.message}</p>}
+                </div>
+
+                <button className={styles.greenButton}>Begin Your Hunting</button>
+            </form>
+        </div>
+)
+}
